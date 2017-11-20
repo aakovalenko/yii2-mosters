a17e17494a802dee11603b5b434be77267e55263
kaaSLDN <kaa89pl@gmail.com>
2017-11-20 11:48:57 +0200

    CRUD Monstertest

--- views/site/about.php
+++ views/site/about.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use app\models\Monstertest;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
@@ -17,3 +18,28 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <code><?= __FILE__ ?></code>
 </div>
+
+<?php
+
+$data = [
+        'name' => 'Wolfman',
+    'age' => 'invalidstring',
+    'gender' => 'male'
+];
+
+$validateMonster1 = new Monstertest($data);
+$validateMonster1->save();
+
+//$deleteMonster = MonsterTest::findOne(['name' => 'Wolfman'])->delete();
+
+$foundMonster1 = MonsterTest::findOne(1);
+$foundMonster2 = MonsterTest::findAll(['gender' => 'm']);
+?>
+
+<hr>
+<p>
+    Found Monster 1 Name: <?= $foundMonster1->name?><br>
+    Found Monster 2 Count: <?= count($foundMonster2)?><br>
+</p>
+<hr>
+
